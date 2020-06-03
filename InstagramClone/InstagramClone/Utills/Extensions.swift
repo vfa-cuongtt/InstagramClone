0f710a025bbabb323dda52fb1d65e1ed0b8265c0
CuongTT <vfa.cuongtt@gmail.com>
2020-06-03 09:08:06 +0700

    UIColor and Anchor Extension

---
+++ InstagramClone/InstagramClone/Utills/Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  Extensions.swift
+//  InstagramClone
+//
+//  Created by vfa on 6/2/20.
+//  Copyright © 2020 Tuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    static func anchor(uiv: UIView, top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) -> Void {
+    
+        // Set Auto layout for textField
+        uiv.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            uiv.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let bottom = bottom {
+            uiv.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let left = left {
+            uiv.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let right = right {
+             
+            uiv.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            uiv.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            uiv.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { 
     
     let btnAddPhoto: UIButton = {
         let button = UIButton(type: .system)
@@ -57,7 +57,7 @@ class ViewController: UIViewController {
         
         let btn = UIButton(type: .system ) // (type: .system) => can tap button
         btn.setTitle("Sign Up", for: .normal)
-        btn.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        btn.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 255)
         btn.layer.cornerRadius = 5
         btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         btn.setTitleColor(.white, for: .normal)
@@ -69,21 +69,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.addSubview(btnAddPhoto)
-        // add active for constraint
-        NSLayoutConstraint.activate([
-            btnAddPhoto.heightAnchor.constraint(equalToConstant: 140),
-            btnAddPhoto.widthAnchor.constraint(equalToConstant: 140),
-            btnAddPhoto.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            btnAddPhoto.topAnchor.constraint(equalTo: view.topAnchor, constant: 40)
-        ])
         
+        // add active for constraint
+        btnAddPhoto.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        btnAddPhoto.anchor(top: view.topAnchor, bottom: nil, left: nil, right: nil, paddingTop: 40, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 140, height: 140)
+        
+        UIView.anchor(uiv: btnAddPhoto, top: view.topAnchor, bottom: nil, left: nil, right: nil, paddingTop: 40, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 140, height: 140)
         setupInputField()
     }
     
     func setupInputField() {
         let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
-        // Set Auto layout for textField
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+
         // phan chia cac  field
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
@@ -91,16 +88,46 @@ class ViewController: UIViewController {
         
         
         view.addSubview(stackView)
-        // add active for constraint
-        NSLayoutConstraint.activate([
-            stackView.heightAnchor.constraint(equalToConstant: 200),
-            stackView.topAnchor.constraint(equalTo: btnAddPhoto.bottomAnchor, constant: 20),
-            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
-            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40)
-      ])
         
+        // add active for constraint
+//        stackView.anchor(top: btnAddPhoto.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingBottom: 0, paddingLeft: 40, paddingRight: 40, width: 0, height: 200)
+        UIView.anchor(uiv: stackView, top: btnAddPhoto.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingBottom: 0, paddingLeft: 40, paddingRight: 40, width: 0, height: 200)
         
     }
 
 }
+
+//extension UIView {
+//    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+//
+//        // Set Auto layout for textField
+//        translatesAutoresizingMaskIntoConstraints = false
+//
+//        if let top = top {
+//            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+//        }
+//
+//        if let bottom = bottom {
+//            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+//        }
+//
+//        if let left = left {
+//            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+//        }
+//
+//        if let right = right {
+//
+//            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+//        }
+//
+//        if width != 0 {
+//            widthAnchor.constraint(equalToConstant: width).isActive = true
+//        }
+//
+//        if height != 0 {
+//            heightAnchor.constraint(equalToConstant: height).isActive = true
+//        }
+//
+//    }
+//}
 
