110562eaa3daf6ce86223515567f05d4db79ae20
CuongTT <vfa.cuongtt@gmail.com>
2020-06-08 00:24:51 +0700

    Feature user profile picture

--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         // Setting auto layout
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        //Add action
+        button.addTarget(self, action: #selector(handleAddPhoto), for: .touchUpInside)
         return button
     }()
     
@@ -28,7 +31,7 @@ class ViewController: UIViewController {
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
         
-        //Add action 
+        //Add action
         tf.addTarget(self, action: #selector(handleTextFieldInputChange), for: .editingChanged)
         return tf
     }()
@@ -114,19 +117,48 @@ class ViewController: UIViewController {
         guard let username = usernameTextField.text, !username.isEmpty else { return }
         guard let password = passwordTextField.text, !password.isEmpty else { return }
         
-        
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
             if let err = error {
                 print("Failed to create user", err)
                 return
             }
             
-            print("Successfully created user", user?.user.uid)
+            guard let image = self.btnAddPhoto.imageView?.image else { return }
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            let filename = NSUUID().uuidString
+            
+            Storage.storage().reference().child("profile_image").child(filename).putData(uploadData, metadata: nil) { (metadata, err) in
+                if let err = err {
+                    print("Failed to upload profile image:", err )
+                    return
+                }
+                
+                guard let profileImageUrl = metadata?.path else {
+                    print("Can't get profile image url")
+                    return
+                }
+                
+                print("Successfully uploaded profile image", profileImageUrl )
+                
+                guard let uid = user?.user.uid else { return }
+                let dictionaryValues = ["username": username, "profileImageUrl": profileImageUrl]
+                let values = [uid: dictionaryValues]
+                 Database.database().reference().child("users").updateChildValues(values) { (err, ref) in
+                     if let err = err {
+                         print("Failed to save user info into db:", err)
+                         return
+                     }
+
+                     print("Successfully saved user info into db")
+                 }
+                
+            }
+            
+            
         }
     }
     
     @objc func handleTextFieldInputChange() {
-        print("AAAAAA")
         guard let email = emailTextField.text, !email.isEmpty, let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
             
             signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 255)
@@ -137,40 +169,33 @@ class ViewController: UIViewController {
         signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
         signUpButton.isEnabled = true
     }
+    
+    @objc func handleAddPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
 
 }
 
-//extension UIView {
-//    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
-//
-//        // Set Auto layout for textField
-//        translatesAutoresizingMaskIntoConstraints = false
-//
-//        if let top = top {
-//            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-//        }
-//
-//        if let bottom = bottom {
-//            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
-//        }
-//
-//        if let left = left {
-//            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
-//        }
-//
-//        if let right = right {
-//
-//            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
-//        }
-//
-//        if width != 0 {
-//            widthAnchor.constraint(equalToConstant: width).isActive = true
-//        }
-//
-//        if height != 0 {
-//            heightAnchor.constraint(equalToConstant: height).isActive = true
-//        }
-//
-//    }
-//}
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage {
+            btnAddPhoto.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            btnAddPhoto.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+         
+        btnAddPhoto.layer.cornerRadius = btnAddPhoto.frame.width/2
+        btnAddPhoto.layer.masksToBounds = true
+        btnAddPhoto.layer.borderColor = UIColor.black.cgColor
+        btnAddPhoto.layer.borderWidth = 2
+        
+        
+        dismiss(animated: true, completion: nil )
+    }
+}
+
 
