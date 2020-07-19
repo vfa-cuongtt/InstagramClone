bd969efb0436157956f77ab4fbbeaae97d0ed883
CuongTT <vfa.cuongtt@gmail.com>
2020-07-19 23:48:55 +0700

    Search Filtering

--- InstagramClone/InstagramClone/SearchView/UserSearchCell.swift
+++ InstagramClone/InstagramClone/SearchView/UserSearchCell.swift
@@ -10,6 +10,13 @@ import UIKit
 
 
 class UserSearchCell: UICollectionViewCell {
+    var user: User? {
+        didSet {
+            usernameLabel.text = user?.username
+            guard let profileImageUrl = user?.profileImageUrl else { return }
+            profileImageView.loadImage(urlString: profileImageUrl)
+        }
+    }
     
     let profileImageView: CustomImageView = {
         let iv = CustomImageView()
--- InstagramClone/InstagramClone/SearchView/UserSearchController.swift
+++ InstagramClone/InstagramClone/SearchView/UserSearchController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 class UserSearchController: UICollectionViewController {
- 
     let cellId = "cellId"
-    let searchBar: UISearchBar = {
+    var users = [User]()
+    var filteredUsers = [User]()
+    
+    lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
         sb.placeholder = "Enter username"
         sb.barTintColor = .gray
 //        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        sb.delegate = self
         return sb
     }()
     
@@ -30,23 +34,64 @@ class UserSearchController: UICollectionViewController {
         UIView.anchor(uiv: searchBar, top: navBar?.topAnchor, bottom: navBar?.bottomAnchor, left: navBar?.leftAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 8, paddingRight: 8, width: 0, height: 0)
         
         collectionView.register(UserSearchCell.self , forCellWithReuseIdentifier: cellId)
-        
-        
         collectionView.alwaysBounceVertical = true
+        
+        fetchUsers()
     }
     
+    fileprivate func  fetchUsers(){
+        let ref = Database.database().reference().child("users")
+        ref.observeSingleEvent(of: .value, with: {(snapshot) in
+            guard let dictionaries = snapshot.value as? [String: Any]
+                else {return}
+            dictionaries.forEach { (key, value) in
+                guard let userDictionary = value as? [String: Any] else { return }
+                
+                let user = User(uid: key, dictionary: userDictionary)
+                self.users.append(user)
+                print(user.uid, user.username)
+            }
+            
+            // Sort users by A-Z a-z
+            self.users.sort { (u1, u2) -> Bool in
+                return u1.username.compare(u2.username) == .orderedAscending
+            }
+            
+            self.filteredUsers = self.users
+            self.collectionView.reloadData()
+        }) { (err) in
+            print( "Failed to fetch users for search ", err )
+        }
+    }
+    
+    // input text auto Search
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredUsers = users
+        } else {
+            self.filteredUsers = self.users.filter({ (user) -> Bool in
+                return user.username.lowercased().contains(searchText.lowercased())
+            })
+        }
+        
+        
+        
+        self.collectionView.reloadData()
+    }
 
    
 }
 
-extension UserSearchController: UICollectionViewDelegateFlowLayout {
+extension UserSearchController: UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return filteredUsers.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-//        cell.backgroundColor = .purple
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! UserSearchCell
+        
+         
+        cell.user = filteredUsers[indexPath.item]
         return cell
     }
     
